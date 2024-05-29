471443369646c425da387ddf45ad0e09884cf21c
moritanuki <ilovfam12@gmail.com>
2024-05-29 15:13:56 +0900

    styleを最小限に

--- README.md
+++ README.md
@@ -48,8 +48,7 @@ onMounted(() => {
 
 4. vueファイルのtemplateは以下のように書く
 ```typescript
-<button type="button" data-twe-ripple-init data-twe-ripple-color="light"
-  class="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">Button</button>
+<button type="button" data-twe-ripple-init data-twe-ripple-color="#ccc" class="rounded border px-7 pb-2.5 pt-3">Button</button>
 ```
 
 ## 注意事項
--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,8 @@ onMounted(() => {
 </script>
 <template>
   <div class="w-full h-screen flex justify-center items-center">
-    <button type="button" data-twe-ripple-init data-twe-ripple-color="light"
-      class="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">Button</button>
+    <button type="button" data-twe-ripple-init data-twe-ripple-color="#ccc"
+      class="rounded border px-7 pb-2.5 pt-3">Button</button>
   </div>
 </template>
 <style scoped></style>
